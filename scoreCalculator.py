8909d2af03664a8358ec418429a3742e35e27ad0
Quinn Goldstein <65020016+quinngoldstein@users.noreply.github.com>
2021-11-10 19:58:52 -1000

    Create scoreCalculator.py
    Added description and header information.

---
+++ scoreCalculator.py
@@ -0,0 +1,4 @@
+# Filename:    scoreCalculator.py
+# Author:      Quinn Goldstein
+# Version:     Version 1
+# Description: This Python program calculates what would be needed to achieve a certain Scorigami in American football. For more information, see nflscorigami.com.
